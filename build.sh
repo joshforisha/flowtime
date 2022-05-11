853ee3e270d5fe00c422880d285893ec63e7e672
Josh Forisha <joshua.forisha@mightynetworks.com>
2022-05-11 10:43:02 -0500

    Move build action into package.json

--- build.sh
+++
@@ -1,1 +0,0 @@
-html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-tag-whitespace --minify-css true --minify-js true _index.html > index.html
